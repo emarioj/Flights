ae2a0c2f1202136b3fdcaf9f107852a6d5e295d8
SubZero <SubZero@DESKTOP-QNAL8EA>
2023-07-14 09:48:51 -0700

    Add project files.

---
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-flights',
+  templateUrl: './search-flights.component.html',
+  styleUrls: ['./search-flights.component.css']
+})
+export class SearchFlightsComponent implements OnInit {
+
+  searchResult: any = [
+    {
+      airline: "American Airlines",
+      remainingNumberOfSeats: 500,
+      departure: { time: Date.now().toString(), place: "Los Angeles" },
+      arrival: { time: Date.now().toString(), place: "Istanbul" },
+      price: "350",
+    },
+    {
+      airline: "Deutsche BA",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "Munchen" },
+      arrival: { time: Date.now().toString(), place: "Schiphol" },
+      price: "600",
+    },
+    {
+      airline: "British Airways",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "London, England" },
+      arrival: { time: Date.now().toString(), place: "Vizzola-Ticino" },
+      price: "600",
+    },
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfseats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
+
+}
